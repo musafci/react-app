3c53142fbd6d0c540036f3f411e3c5174b4bbdce
Md. Abu Musa <musa.fci@gmail.com>
2022-08-14 00:50:59 +0600

    Commit#2 State & Lifecycle

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,33 +1,31 @@
 import React, {Component} from "react";
 class App extends Component {
     
-    // constructor(props) {
-    //     super(props);
-    //     this.count = 5;
-    //     this.state = {
-    //         count: 0
-    //     }
-    // }
+    state = {
+        count: 0,
+    }
 
-    // count = 10;
-
-    render() {
-        
-        // console.log("count", this.state.count);
+    render() {        
 
         return(
             <div className="CustomClass">
+
                 <h1>State</h1>
                 <h2>Count: {this.state.count}</h2>
 
                 <button onClick={() => {
-                    // this.count++
-                    this.setState({count: this.state.count + 1});
 
-                    console.log("clicked", this.state.count);
+                    this.setState((prev) => {
+                        return {
+                            count: prev.count+1
+                        }
+                    }, () => {
+                        console.log("clicked", this.state.count);
+                    });                    
 
                     }}>Add + 1
                 </button>
+
             </div>
         )
     }
