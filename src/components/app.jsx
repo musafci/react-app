87eb1083a083ebf1f4b263d3dd38e2cd545db8bd
Md. Abu Musa <musa.fci@gmail.com>
2022-08-14 03:00:19 +0600

    Simple react app no#1 Timer Count

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,30 +2,64 @@ import React, {Component} from "react";
 class App extends Component {
     
     state = {
-        count: 0,
+        count: 0
     }
 
-    render() {        
+    intervalId = null;
+
+    incrementCount = () => {
+        this.setState({count: this.state.count +1});
+    };
+
+    decrementCount = () => {
+        if(this.state.count > 0) {
+            this.setState({count: this.state.count -1});
+        }
+    };
+
+    startTimer = () => {
+        if(this.state.count > 0 && !this.intervalId) {
+            this.intervalId =  setInterval(() => {
+                this.setState({count: this.state.count -1}, () => {
+                    if(this.state.count === 0) {
+                        alert("Counter Finished.")
+                        clearInterval(this.intervalId)
+                        this.intervalId = null
+                    }
+                })
+            }, 1000)
+        }
+    };
+
+    stopTimer = () => {
+        if(this.intervalId) {
+            clearInterval(this.intervalId)
+            this.intervalId = null
+        }
+    };
+
+    resetTimer = () => {
+        this.setState({count: 0})
+        clearInterval(this.intervalId)
+        this.intervalId = null
+    };
+
+    render() {
 
         return(
             <div className="CustomClass">
+                <div>
+                    <h1>Simple Timer</h1>
+                    <button onClick={this.incrementCount}>+</button>
+                    <span>{this.state.count}</span>
+                    <button onClick={this.decrementCount}>-</button>
+                </div>
 
-                <h1>State</h1>
-                <h2>Count: {this.state.count}</h2>
-
-                <button onClick={() => {
-
-                    this.setState((prev) => {
-                        return {
-                            count: prev.count+1
-                        }
-                    }, () => {
-                        console.log("clicked", this.state.count);
-                    });                    
-
-                    }}>Add + 1
-                </button>
-
+                <div>
+                    <button onClick={this.startTimer}>Start</button>
+                    <button onClick={this.stopTimer}>Stop</button>
+                    <button onClick={this.resetTimer}>Reset</button>
+                </div>                
             </div>
         )
     }
