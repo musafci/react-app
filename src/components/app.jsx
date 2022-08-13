29c5cff28147a049fd77edfecff74b0a1ba5f0e8
Md. Abu Musa <musa.fci@gmail.com>
2022-08-13 23:22:54 +0600

    Commit#1 State & Lifecycle

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,19 +1,33 @@
 import React, {Component} from "react";
-import Profile from "./profile";
-// import Skills from "./profile/skills";
-// import LearnProps from "./props";
-
 class App extends Component {
+    
+    // constructor(props) {
+    //     super(props);
+    //     this.count = 5;
+    //     this.state = {
+    //         count: 0
+    //     }
+    // }
+
+    // count = 10;
+
     render() {
+        
+        // console.log("count", this.state.count);
+
         return(
             <div className="CustomClass">
-                <Profile/>
+                <h1>State</h1>
+                <h2>Count: {this.state.count}</h2>
 
-                {/* <div>
-                    <LearnProps name="musa"/>
-                    <LearnProps name="rana"/>
-                    <Skills skillA={"PHP"} skillB={"Laravel"} skillC={"Vue.js"}/>
-                </div> */}
+                <button onClick={() => {
+                    // this.count++
+                    this.setState({count: this.state.count + 1});
+
+                    console.log("clicked", this.state.count);
+
+                    }}>Add + 1
+                </button>
             </div>
         )
     }
